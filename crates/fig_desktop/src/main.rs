mangaohua/amazon-q-developer-cli

commit c13e14ba8e0faba7954c9f0cdde2c9e4857bc516

build: update to rust 1.84.0 (#394)
* build: update to rust 1.84.0

* fix more lints

crates/fig_desktop/src/main.rs
@@ -16,7 +16,7 @@ mod utils;
 mod webview;
 
 use std::path::Path;
-use std::process::exit;
+use std::process::ExitCode;
 use std::sync::{
     Arc,
     RwLock,
@@ -84,7 +84,7 @@ pub type EventLoopProxy = WryEventLoopProxy<Event>;
 pub type EventLoopWindowTarget = WryEventLoopWindowTarget<Event>;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> ExitCode {
     let cli = cli::Cli::parse();
 
     let _log_guard = initialize_logging(LogArgs {
@@ -109,33 +109,13 @@ async fn main() {
     }
 
     if cli.is_startup && !fig_settings::settings::get_bool_or("app.launchOnStartup", true) {
-        std::process::exit(0);
+        return ExitCode::SUCCESS;
     }
 
-    let page = cli
-        .url_link
-        .as_ref()
-        .and_then(|url| match Url::parse(url) {
-            Ok(url) => Some(url),
-            Err(err) => {
-                error!(%err, %url, "Failed to parse url");
-                exit(1)
-            },
-        })
-        .and_then(|url| {
-            if url.scheme() != URL_SCHEMA {
-                error!(scheme = %url.scheme(), %url, "Invalid scheme");
-                exit(1)
-            }
-
-            url.host_str().and_then(|s| match s {
-                "dashboard" => Some(url.path().to_owned()),
-                _ => {
-                    error!("Invalid deep link");
-                    None
-                },
-            })
-        });
+    let page = match parse_url_page(cli.url_link.as_deref()) {
+        Ok(page) => page,
+        Err(exit_code) => return exit_code,
+    };
 
     if !cli.allow_multiple {
         match get_current_pid() {
@@ -157,21 +137,10 @@ async fn main() {
                     )
                     .show();
 
-                return;
+                return ExitCode::FAILURE;
             }
         }
     }
-
-    // tokio::spawn(async {
-    //     fig_telemetry::emit_track(fig_telemetry::TrackEvent::new(
-    //         fig_telemetry::TrackEventType::LaunchedApp,
-    //         fig_telemetry::TrackSource::Desktop,
-    //         env!("CARGO_PKG_VERSION").into(),
-    //         empty::<(&str, &str)>(),
-    //     ))
-    //     .await
-    //     .ok();
-    // });
 
     let ctx = Context::new();
     install::run_install(Arc::clone(&ctx), cli.ignore_immediate_update).await;
@@ -226,18 +195,49 @@ async fn main() {
 
     webview_manager.run().await.unwrap();
     fig_telemetry::finish_telemetry().await;
+    ExitCode::SUCCESS
+}
+
+fn parse_url_page(url: Option<&str>) -> Result<Option<String>, ExitCode> {
+    let Some(url) = url else {
+        return Ok(None);
+    };
+
+    let url = match Url::parse(url) {
+        Ok(url) => url,
+        Err(err) => {
+            error!(%err, %url, "Failed to parse url");
+            return Err(ExitCode::FAILURE);
+        },
+    };
+
+    if url.scheme() != URL_SCHEMA {
+        error!(scheme = %url.scheme(), %url, "Invalid scheme");
+        return Err(ExitCode::FAILURE);
+    }
+
+    Ok(url.host_str().and_then(|s| match s {
+        "dashboard" => Some(url.path().to_owned()),
+        _ => {
+            error!("Invalid deep link");
+            None
+        },
+    }))
 }
 
 #[cfg(target_os = "linux")]
-async fn allow_multiple_running_check(current_pid: sysinfo::Pid, kill_old: bool, page: Option<String>) {
+async fn allow_multiple_running_check(
+    current_pid: sysinfo::Pid,
+    kill_old: bool,
+    page: Option<String>,
+) -> Option<ExitCode> {
     use std::ffi::OsString;
 
     use tracing::debug;
 
     if kill_old {
         eprintln!("Option kill-old is not supported on Linux.");
-        #[allow(clippy::exit)]
-        exit(0);
+        return Some(ExitCode::SUCCESS);
     }
 
     let system = System::new_with_specifics(
@@ -278,16 +278,20 @@ async fn allow_multiple_running_check(current_pid: sysinfo::Pid, kill_old: bool,
                     eprintln!("Failed to open Fig: {err}");
                 }
 
-                #[allow(clippy::exit)]
-                exit(0);
+                return Some(ExitCode::SUCCESS);
             },
             _ => (),
         }
     }
+    None
 }
 
 #[cfg(target_os = "macos")]
-async fn allow_multiple_running_check(current_pid: sysinfo::Pid, kill_old: bool, page: Option<String>) {
+async fn allow_multiple_running_check(
+    current_pid: sysinfo::Pid,
+    kill_old: bool,
+    page: Option<String>,
+) -> Option<ExitCode> {
     use std::ffi::OsString;
 
     let app_process_name = OsString::from(APP_PROCESS_NAME);
@@ -341,21 +345,21 @@ async fn allow_multiple_running_check(current_pid: sysinfo::Pid, kill_old: bool,
                     {
                         eprintln!("Failed to open Fig: {err}");
                     }
-
-                    #[allow(clippy::exit)]
-                    exit(0);
                 };
 
                 match (process.user_id().map(|uid| uid as &u32), current_user_id.as_ref()) {
                     (Some(uid), Some(current_uid)) if uid == current_uid => {
                         on_match.await;
+                        return Some(ExitCode::SUCCESS);
                     },
                     (_, None) => {
                         on_match.await;
+                        return Some(ExitCode::SUCCESS);
                     },
                     _ => {},
                 }
             }
         }
     }
+    None
 }

crates/figterm/src/main.rs
@@ -320,9 +320,9 @@ where
         Some(at) => {
             let lock_expired = at.elapsed().unwrap_or(Duration::ZERO) > Duration::from_millis(16);
             let should_unlock = lock_expired
-                || term.get_current_buffer().map_or(true, |buff| {
-                    &buff.buffer == (&EXPECTED_BUFFER.lock().unwrap() as &String)
-                });
+                || term
+                    .get_current_buffer()
+                    .is_none_or(|buff| &buff.buffer == (&EXPECTED_BUFFER.lock().unwrap() as &String));
             if should_unlock {
                 handle.take();
                 if lock_expired {

.github/workflows/rust.yml
@@ -29,7 +29,7 @@ jobs:
       - name: Install dependencies (macOS)
         if: runner.os == 'Macos'
         run: brew install protobuf fish shellcheck
-      - uses: dtolnay/rust-toolchain@1.83.0
+      - uses: dtolnay/rust-toolchain@1.84.0
         id: toolchain
         with:
           components: clippy

crates/alacritty_terminal/src/ansi.rs
@@ -109,10 +109,7 @@ fn parse_number(input: &[u8]) -> Option<u8> {
     for c in input {
         let c = *c as char;
         if let Some(digit) = c.to_digit(10) {
-            num = match num.checked_mul(10).and_then(|v| v.checked_add(digit as u8)) {
-                Some(v) => v,
-                None => return None,
-            }
+            num = num.checked_mul(10).and_then(|v| v.checked_add(digit as u8))?;
         } else {
             return None;
         }
@@ -1012,7 +1009,7 @@ where
         match (action, intermediates) {
             ('s', [b'=']) => {
                 // Start a synchronized update. The end is handled with a separate parser.
-                if params.iter().next().map_or(false, |param| param[0] == 1) {
+                if params.iter().next().is_some_and(|param| param[0] == 1) {
                     self.state.dcs = Some(Dcs::SyncStart);
                 }
             },

crates/amzn-toolkit-telemetry/src/config/endpoint.rs
@@ -5,7 +5,6 @@ pub use ::aws_smithy_runtime_api::client::endpoint::{
 };
 pub use ::aws_smithy_types::endpoint::Endpoint;
 
-///
 #[cfg(test)]
 mod test {}
 

crates/fig_api_client/src/error.rs
@@ -55,14 +55,14 @@ impl Error {
     pub fn is_throttling_error(&self) -> bool {
         match self {
             Error::Credentials(_) => false,
-            Error::GenerateCompletions(e) => e.as_service_error().map_or(false, |e| e.is_throttling_error()),
-            Error::GenerateRecommendations(e) => e.as_service_error().map_or(false, |e| e.is_throttling_error()),
-            Error::ListAvailableCustomizations(e) => e.as_service_error().map_or(false, |e| e.is_throttling_error()),
-            Error::ListAvailableServices(e) => e.as_service_error().map_or(false, |e| e.is_throttling_error()),
+            Error::GenerateCompletions(e) => e.as_service_error().is_some_and(|e| e.is_throttling_error()),
+            Error::GenerateRecommendations(e) => e.as_service_error().is_some_and(|e| e.is_throttling_error()),
+            Error::ListAvailableCustomizations(e) => e.as_service_error().is_some_and(|e| e.is_throttling_error()),
+            Error::ListAvailableServices(e) => e.as_service_error().is_some_and(|e| e.is_throttling_error()),
             Error::CodewhispererGenerateAssistantResponse(e) => {
-                e.as_service_error().map_or(false, |e| e.is_throttling_error())
+                e.as_service_error().is_some_and(|e| e.is_throttling_error())
             },
-            Error::QDeveloperSendMessage(e) => e.as_service_error().map_or(false, |e| e.is_throttling_error()),
+            Error::QDeveloperSendMessage(e) => e.as_service_error().is_some_and(|e| e.is_throttling_error()),
             Error::CodewhispererChatResponseStream(_)
             | Error::QDeveloperChatResponseStream(_)
             | Error::SmithyBuild(_)

crates/fig_auth/src/builder_id.rs
@@ -547,7 +547,7 @@ pub async fn refresh_token() -> Result<Option<BuilderIdToken>> {
 }
 
 pub async fn is_amzn_user() -> Result<bool> {
-    Ok(builder_id_token().await?.map_or(false, |t| t.is_amzn_user()))
+    Ok(builder_id_token().await?.is_some_and(|t| t.is_amzn_user()))
 }
 
 pub async fn is_logged_in() -> bool {

crates/fig_desktop/src/platform/macos.rs
@@ -489,7 +489,7 @@ impl PlatformStateImpl {
                     // Checking if IME is installed is async :(
                     let enabled_proxy = self.proxy.clone();
                     tokio::spawn(async move {
-                        let is_terminal_disabled = current_terminal.as_ref().map_or(false, |terminal| {
+                        let is_terminal_disabled = current_terminal.as_ref().is_some_and(|terminal| {
                             fig_settings::settings::get_bool_or(
                                 format!("integrations.{}.disabled", terminal.internal_id()),
                                 false,
@@ -558,7 +558,7 @@ impl PlatformStateImpl {
                     let dashboard_visible = dashboard_visible.unwrap_or_else(|| {
                         window_map
                             .get(&DASHBOARD_ID)
-                            .map_or(false, |window| window.window.is_visible())
+                            .is_some_and(|window| window.window.is_visible())
                     });
 
                     if dashboard_visible {

commit e2e5538707f8c97d046af402c89d94295e9f6dbc

fix(fig_desktop): exit code not propagated by main (#409)
This fixes an issues that was accidently introduced in c13e14b. To prevent this again I have marked the functions in the file as `#[must_use]`.

crates/fig_desktop/src/main.rs
@@ -120,7 +120,9 @@ async fn main() -> ExitCode {
     if !cli.allow_multiple {
         match get_current_pid() {
             Ok(current_pid) => {
-                allow_multiple_running_check(current_pid, cli.kill_old, page.clone()).await;
+                if let Some(exit_code) = allow_multiple_running_check(current_pid, cli.kill_old, page.clone()).await {
+                    return exit_code;
+                }
             },
             Err(err) => warn!(%err, "Failed to get pid"),
         }
@@ -226,6 +228,7 @@ fn parse_url_page(url: Option<&str>) -> Result<Option<String>, ExitCode> {
 }
 
 #[cfg(target_os = "linux")]
+#[must_use]
 async fn allow_multiple_running_check(
     current_pid: sysinfo::Pid,
     kill_old: bool,
@@ -287,6 +290,7 @@ async fn allow_multiple_running_check(
 }
 
 #[cfg(target_os = "macos")]
+#[must_use]
 async fn allow_multiple_running_check(
     current_pid: sysinfo::Pid,
     kill_old: bool,
@@ -297,14 +301,7 @@ async fn allow_multiple_running_check(
     let app_process_name = OsString::from(APP_PROCESS_NAME);
     let system = System::new_with_specifics(RefreshKind::new().with_processes(ProcessRefreshKind::new()));
     let processes = system.processes_by_name(&app_process_name);
-
-    cfg_if::cfg_if! {
-        if #[cfg(unix)] {
-            let current_user_id = Some(nix::unistd::getuid().as_raw());
-        } else {
-            let current_user_id = None;
-        }
-    };
+    let current_uid = nix::unistd::getuid().as_raw();
 
     for process in processes {
         let pid = process.pid();
@@ -347,12 +344,8 @@ async fn allow_multiple_running_check(
                     }
                 };
 
-                match (process.user_id().map(|uid| uid as &u32), current_user_id.as_ref()) {
-                    (Some(uid), Some(current_uid)) if uid == current_uid => {
-                        on_match.await;
-                        return Some(ExitCode::SUCCESS);
-                    },
-                    (_, None) => {
+                match process.user_id().map(|uid| uid as &u32) {
+                    Some(&uid) if uid == current_uid => {
                         on_match.await;
                         return Some(ExitCode::SUCCESS);
                     },